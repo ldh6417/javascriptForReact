6c7e4daf790b373a456315463a93d0494d9454a3
ldh6417 <ldh85316417@gmail.com>
2026-02-05 10:19:54 +0900

    [생성] 스프레드연산자 rest 매개변수

---
+++ section01/chapter01/chapter26.js
@@ -0,0 +1,43 @@
+//1. spread 연산자
+let array1 = [1,2,3,4,5]
+let array2 = [10,...array1,20,30]
+let array3 = array1 // 얕은복사
+let array4 = [...array1]     //깊은 복사
+
+//console.log(array4);
+
+//2. 객체생성 (spread 연산자 활용)깊은복사
+let obj1 = { 
+  a: 1, 
+  b: 2, 
+}; 
+let obj2 = {
+  k:10,
+  ...obj1,
+  c:4,
+  d:5
+}
+//console.log(obj2);
+
+//3구조분해할당, 스프레드 연산자
+
+function funcA([p1,p2,p3]){
+  console.log(p1+10,p2*10,p3/10);
+}
+const array5 = [1,2,3];
+funcA(array5);
+
+//3.1 스프레드 연산자
+function funcB(p1,p2,p3){
+  console.log(p1+10,p2*10,p3/10);
+}
+const array6 = [1,2,3];
+funcB(...array6);
+
+//rest 매개변수
+const array7 = [11,12,13]
+function funcC(p1,...rest){
+  console.log(p1,)
+  console.log(rest)
+}
+funcC(...array7);
