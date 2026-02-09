61423010622206868e9c5fd3297fdf02425ac75d
ldh6417 <ldh85316417@gmail.com>
2026-02-09 11:48:24 +0900

    [생성] 사용자 정의하기 useState

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -3,13 +3,15 @@ import { useState } from 'react'
 import Bulb from './components/Bulb';
 import Count from './components/Count';
 import Register from './components/Register';
+import HookExam from './components/HookExam';
+
 
 
 export default function  App() {
   
 
   return <>
-  <Register/>
+  <HookExam/>
   </>
 
 
